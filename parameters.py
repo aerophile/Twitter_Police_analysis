13a86e8f478da477901c90808d98f952e99a8036
shubham <shubham0075@gmail.com>
2017-02-01 23:20:47 +0530

    kolkata

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 
 from api_keys import *
 # police twitter handles for obtaining tweets
-city ={'BlrCityPolice':"Bangalore",'MumbaiPolice':"Mumbai",'DelhiPolice':"Delhi",'kolkataPolice':"Kolkatta",'hydcitypolice':"Hyderabad"}
+city ={'BlrCityPolice':"Bangalore",'MumbaiPolice':"Mumbai",'DelhiPolice':"Delhi",'kolkataPolice':"Kolkata",'hydcitypolice':"Hyderabad"}
 handles = ['BlrCityPolice','MumbaiPolice','DelhiPolice','kolkataPolice','hydcitypolice']
 
 #number of tweets to obtain
